2d4c8e9f697bccb0d91039b2c94db87217645c82
robin moussu <robin.moussu+git@gmail.com>
2014-11-11 15:14:57 +0100

    Suppression du type ennuméré du robot

--- src/TypeRobot.java
+++
@@ -1,8 +0,0 @@
-// Déclaration de l'énumération indiquant le type des robots
-// Dernière modification:  Thibaud BACKENSTRASS, 9 novembre
-public enum TypeRobot {
-    DRONE,
-    ROUES,
-    CHENILLES,
-    PATTES
-}

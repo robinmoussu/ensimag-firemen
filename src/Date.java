595ecf5a2bc92f4277302d27098299ec419a91c4
thibaudbackenstrass <thibaud.backenstrass@phelma.grenoble-inp.fr>
2014-11-17 19:49:28 +0100

    Ajout de la javadoc sur DonneesSimulation.java

--- src/Date.java
+++ src/Date.java
@@ -1,33 +1,58 @@
 public class Date {
     private long date;
     
-    // Constructeur
+    /**
+     * Constructeur de date nulle.
+     */
     public Date() {
         this.date = 0;
     }
+    /**
+     * Constructeur de date.
+     * @param date Date à laquelle initialiser l'objet-date
+     */
     public Date(long date) {
         this.date = date;
     }
+
     
-    // Accesseur
+    /**
+     * Accesseur sur la date.
+     * @return Date courante (en secondes)
+     */
     public long getDate() {
         return this.date;
     }
+
     
-    // Incrémente la date
+    /**
+     * Incrémente la date d'un certain nombre de secondes.
+     * @param t Nombre d'unités à ajouter à la date courante
+     */
     public void incrementeDate(long t) {
         this.date = date + t;
     }
+    /**
+     * Incrémente la date d'une seconde.
+     */
     public void incrementeDate() {
         incrementeDate(1);
     }
 
-    // Réinitialise la date courante
+
+    /**
+     * Réinitialiser la date courante à la date nulle.
+     */
     public void resetDate() {
         this.date = 0;
     }
     
-    // Retourne une date incrémentée de t
+
+    /**
+     * Retourne la date courante incrémentée d'un certain nombre de secondes, mais sans modifier la date courante.
+     * @param t Nombre d'unités à ajouter à la date courante
+     * @return Date courante incrémentée de t secondes
+     */
     public Date dateInc(long t) {
         return new Date(this.date + t);
     }
--- src/DonneesSimulation.java
+++ src/DonneesSimulation.java
@@ -1,50 +1,100 @@
-// Classe de description des données de la simulation
-// Dernière modification : Thibaud BACKENSTRASS, 9 novembre
 import java.util.ArrayList;
+
 public class DonneesSimulation {
-    // Attributs
     private Carte carte;
     private ArrayList<Incendie> incendies;
     private ArrayList<Robot> robots;
 
-    // Constructeur
+    /**
+     * Constructeur des données de la simulation.
+     * @param nbLignes Nombre de lignes de la simulation
+     * @param nbColonnes Nombre de colonnes de la simulation
+     * @param tailleCases Taille (en mètres) des cases de la simulation
+     * @throws ConstructionException si l'un des paramètres est négatif ou nul
+     */
     public DonneesSimulation(int nbLignes, int nbColonnes, int tailleCases) throws ConstructionException {
         carte = new Carte(nbLignes, nbColonnes, tailleCases);
         incendies = new ArrayList<Incendie>();
         robots = new ArrayList<Robot>();
     }
 
-    // Accesseurs
+
+    /**
+     * Accesseur sur la carte.
+     * @return Référence sur la carte de la simulation
+     */
     public Carte getCarte() {
         return this.carte;
     }
+    /**
+     * Accesseursur la liste des incendies.
+     * @return ArrayList contenant les incendies
+     */
     public ArrayList<Incendie> getIncendies() {
         return this.incendies;
     }
+    /**
+     * Accesseur sur le nombre d'incendies.
+     * @return Nombre d'incendies de la simulation
+     */
     public int getNbIncendies() {
         return this.incendies.size();
     }
+    /**
+     * Accesseur sur la liste des robots.
+     * @return ArrayList contenant les robots
+     */
     public ArrayList<Robot> getRobots() {
         return this.robots;
     }
+    /**
+     * Accesseur sur le nombre de robots.
+     * @return Nombre de robots de la simulation
+     */
     public int getNbRobots() {
         return this.robots.size();
     }
+    /**
+     * Accesseur sur le nombre de lignes de la simulation.
+     * @return Nombre de lignes de la simulation
+     */
     public int getNbLignes() {
         return carte.getNbLignes();
     }
+    /**
+     * Accesseur sur le nombre de colonnes de la simulation.
+     * @return Nombre de colonnes de la simulation
+     */
     public int getNbColonnes() {
         return carte.getNbColonnes();
     }
 
-    // Méthodes pour ajouter des éléments à la simulation
+
+    /**
+     * Ajouter une case à la simulation, à la place (ligne,colonne) à partir du coin supérieur gauche.
+     * @param ligne Numéro de la ligne où créer la case, à partir de zéro
+     * @param colonne Numéro de la colonne où créer la case, à partir de zéro
+     * @param t Nature du terrain sur la case à créer
+     * @throws ConstructionException si les coordonnées sont à l'extérieur des limites de la carte
+     */
     public void addCase(int ligne, int colonne, NatureTerrain t) throws ConstructionException {
         this.carte.setCase(ligne, colonne, t);
     }
-    public void addCase(Case case_) throws ConstructionException {
-        this.carte.setCase(case_);
+    /**
+     * Ajouter une case à la simulation.
+     * @param c Référence sur la case à ajouter à la simulation
+     * @throws ConstructionException si la case est en-dehors des limites de la carte
+     */
+    public void addCase(Case c) throws ConstructionException {
+        this.carte.setCase(c);
     }
     
+
+    /**
+     * Indique si une case est en-dehors de la carte.
+     * @param pos Référence sur la case à tester
+     * @return Booléen indiquant si la case est en-dehors des limites de la carte
+     */
     protected boolean estHorsCarte(Case pos) {
         int ligne   = pos.getLigne();
         int colonne = pos.getColonne();
@@ -54,21 +104,35 @@ public class DonneesSimulation {
                 
     }
     
+
+    /**
+     * Ajouter un incendie à la simulation.
+     * @param pos Référence sur une case indiquant la position de l'incendie
+     * @param eau Intensité de l'incendie, en litres d'eau nécessaires pour l'éteindre
+     * @throws ConstructionException si la position de l'incendie est hors des limites de la carte
+     */
     public void addIncendie(Case pos, int eau) throws ConstructionException {
         // Si la position est en-dehors de la carte...
         if (estHorsCarte(pos)) {
-            throw new ConstructionException("Impossible de créer un incendie"
-                    + "sur la case spécifiée !");
+            throw new ConstructionException("Impossible de créer un incendie sur la case spécifiée !");
         }
 
         incendies.add(new Incendie(pos, eau));
     }
+
+
+    /**
+     * Ajouter un robot à la simulation.
+     * @param pos Référence sur une case indiquant la position initiale du robot
+     * @param robot Référence sur le robot à créer
+     * @throws ConstructionException si la position du robot est hors des limites de la carte
+     */
     public void addRobot(Case pos, Robot robot) throws ConstructionException {
         // Si la position est en-dehors de la carte...
         if (estHorsCarte(pos)) {
-            throw new ConstructionException("Impossible de créer un robot sur"
-                    + "la case spécifiée !");
+            throw new ConstructionException("Impossible de créer un robot sur la case spécifiée !");
         }
+
         robot.setPosition(pos);
         robots.add(robot);
     }

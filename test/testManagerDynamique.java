87436d339c6e718c695bcb1faf02cb77851eb222
robin moussu <robin.moussu+git@gmail.com>
2014-11-18 18:49:13 +0100

    add test ChercherEauMultiple

--- test/testManagerDynamique.java
+++ test/testManagerDynamique.java
@@ -46,45 +46,92 @@ public class testManagerDynamique {
             eau = new Case(x, y + distanceEau, NatureTerrain.EAU);
             data.addCase(eau);
             
-            // initialisation d'un robot vide
+            // initialisation du robot
             robot  = new RobotRoues(depart);
             data.addRobot(depart, robot);
-            
-            while(!robot.estVide()) {
-                robot.deverserEau(data, 10);
-            }
             
             manager = new ManagerDynamique(simu, data);
             
             ///////////////////////////////////////////////////////////////////
             
-            
-            assertThat("Le robot doit initialement être initialement vide",
-                    true, is(robot.estVide()));
-            
-            // Le robot est situé à une distance de distanceEau -1 unitées du
-            // bord d'eau
-            for (int i = 0; i < distanceEau - 1; i++) {
-                Case prev = robot.getPosition();
-                manager.manage();
-                assertThat("Le robot doit se deplacer", prev,
-                        is(not(robot.getPosition())));
-                System.out.println(robot.getPosition());
-            }
-            
-            assertThat("Le robot doit être à proximitée de l'eau. "
-                    + "Position courante " + robot.getPosition() + ". "
-                    + "Position de l'eau " + eau, true,
-                    is(data.getCarte().estBordEau(robot.getPosition())));
-            
-            manager.manage();
-            assertThat("Le robot doit être désormais remplis", true,
-                    is(robot.estPlein()));
+            scenarioChercherEau(data, robot, distanceEau, manager);
         } catch (ConstructionException | SimulationException ex) {
             Logger.getLogger(testManagerDynamique.class.getName()).log(
                     Level.SEVERE, null, ex);
             fail(ex.toString());
         }
+    }
+    
+    @Test
+    public void chercheEauMultiple() {
+        try {
+            Robot robot;
+            ManagerDynamique manager;
+            Simulateur simu;
+            DonneesSimulation data;
+            Case depart, eau1, eau2, eau3, eau4;
+            
+            simu = new Simulateur(new Date());
+            data = new DonneesSimulation(1000, 1000, 1000);
+            
+            // initalisation de la carte
+            int distanceEau = 10;
+            int x = 1;
+            int y = 1;
+            depart = data.getCarte().getCase(x, y);
+            eau1 = new Case(x, y + distanceEau, NatureTerrain.EAU);
+            eau2 = new Case(x + 1, y + distanceEau, NatureTerrain.EAU);
+            eau3 = new Case(x + distanceEau, y+distanceEau, NatureTerrain.EAU);
+            eau4 = new Case(x + distanceEau, y, NatureTerrain.EAU);
+            data.addCase(eau1);
+            data.addCase(eau2);
+            data.addCase(eau3);
+            data.addCase(eau4);
+            
+            // initialisation d'un robot vide
+            robot  = new RobotRoues(depart);
+            data.addRobot(depart, robot);
+            
+            manager = new ManagerDynamique(simu, data);
+            
+            ///////////////////////////////////////////////////////////////////
+            
+            scenarioChercherEau(data, robot, distanceEau, manager);
+        } catch (ConstructionException | SimulationException ex) {
+            Logger.getLogger(testManagerDynamique.class.getName()).log(
+                    Level.SEVERE, null, ex);
+            fail(ex.toString());
+        }
+    }
+    
+    ///////////////////////////////////////////////////////////////////////////
+    
+    void scenarioChercherEau(DonneesSimulation data, Robot robot,
+            int distanceEau, Manager manager)
+            throws SimulationException
+    {
+        while (!robot.estVide()) {
+            robot.deverserEau(data, 10);
+        }
+        assertThat("Le robot doit initialement être initialement vide",
+                true, is(robot.estVide()));
 
+        // Le robot est situé à une distance de distanceEau -1 unitées du
+        // bord d'eau
+        for (int i = 0; i < distanceEau - 1; i++) {
+            Case prev = robot.getPosition();
+            manager.manage();
+            assertThat("Le robot doit se deplacer", prev,
+                    is(not(robot.getPosition())));
+            System.out.println(robot.getPosition());
+        }
+
+        assertThat("Le robot doit être à proximitée de l'eau. "
+                + "Position courante " + robot.getPosition() + ". ", true,
+                is(data.getCarte().estBordEau(robot.getPosition())));
+
+        manager.manage();
+        assertThat("Le robot doit être désormais remplis", true,
+                is(robot.estPlein()));
     }
 }

54a0874b66e3fac257cc4ad22e8abef47dbe4dba
robin moussu <robin.moussu+git@gmail.com>
2014-11-18 20:30:12 +0100

    clean up test and add eteindreFeuRobotVide

--- test/testManagerDynamique.java
+++ test/testManagerDynamique.java
@@ -38,17 +38,13 @@ public class testManagerDynamique {
             simu = new Simulateur(new Date());
             data = new DonneesSimulation(1000, 1000, 1000);
             
-            // initalisation de la carte
-            int distanceEau = 10;
-            int x = 1;
-            int y = 1;
-            depart = data.getCarte().getCase(x, y);
-            eau = new Case(x, y + distanceEau, NatureTerrain.EAU);
-            data.addCase(eau);
+            depart = data.getCarte().getCase(1,1);
+            int distanceEau = initCarte1Eau(data, depart);
             
             // initialisation du robot
             robot  = new RobotRoues(depart);
             data.addRobot(depart, robot);
+            viderRobot(data, robot);
             
             manager = new ManagerDynamique(simu, data);
             
@@ -69,28 +65,19 @@ public class testManagerDynamique {
             ManagerDynamique manager;
             Simulateur simu;
             DonneesSimulation data;
-            Case depart, eau1, eau2, eau3, eau4;
-            
+            Case depart;
+            int distanceEau;
+
             simu = new Simulateur(new Date());
             data = new DonneesSimulation(1000, 1000, 1000);
-            
-            // initalisation de la carte
-            int distanceEau = 10;
-            int x = 1;
-            int y = 1;
-            depart = data.getCarte().getCase(x, y);
-            eau1 = new Case(x, y + distanceEau, NatureTerrain.EAU);
-            eau2 = new Case(x + 1, y + distanceEau, NatureTerrain.EAU);
-            eau3 = new Case(x + distanceEau, y+distanceEau, NatureTerrain.EAU);
-            eau4 = new Case(x + distanceEau, y, NatureTerrain.EAU);
-            data.addCase(eau1);
-            data.addCase(eau2);
-            data.addCase(eau3);
-            data.addCase(eau4);
+
+            depart = data.getCarte().getCase(1,1);
+            distanceEau = initCarteMultipleEau(data, depart);
             
             // initialisation d'un robot vide
             robot  = new RobotRoues(depart);
             data.addRobot(depart, robot);
+            viderRobot(data, robot);
             
             manager = new ManagerDynamique(simu, data);
             
@@ -117,17 +104,8 @@ public class testManagerDynamique {
             simu = new Simulateur(new Date());
             data = new DonneesSimulation(1000, 1000, 1000);
             
-            // initalisation de la carte
-            distanceEau = 10;
-            distanceFeu = 15;
-            int x = 1;
-            int y = 1;
-            depart = data.getCarte().getCase(x, y);
-            eau = new Case(x, y + distanceEau, NatureTerrain.EAU);
-            data.addCase(eau);
-            feu = new Case(x + distanceFeu, y, NatureTerrain.HABITAT);
-            data.addCase(feu);
-            data.addIncendie(feu, 1); // On initialise un feu simple à éteindre
+            depart = data.getCarte().getCase(1, 1);
+            distanceFeu = initCarte1Feu(data, depart);
             
             // initialisation du robot
             robot  = new RobotRoues(depart);
@@ -147,18 +125,55 @@ public class testManagerDynamique {
 
     }
     
+    @Test
+    public void eteindreFeuRobotVide() {
+        try {
+            Robot robot;
+            ManagerDynamique manager;
+            Simulateur simu;
+            DonneesSimulation data;
+            Case depart, eau;
+            
+            simu = new Simulateur(new Date());
+            data = new DonneesSimulation(1000, 1000, 1000);
+            
+            depart = data.getCarte().getCase(1,1);
+            int distanceEau = initCarte1Eau(data, depart);
+            
+            // initialisation du robot
+            robot  = new RobotRoues(depart);
+            data.addRobot(depart, robot);
+            viderRobot(data, robot);
+            
+            manager = new ManagerDynamique(simu, data);
+            
+            ///////////////////////////////////////////////////////////////////
+            
+            scenarioChercherEau(data, robot, distanceEau, manager);
+            scenarioChercherFeu(data, robot, distanceEau, manager);
+            
+        } catch (ConstructionException | SimulationException ex) {
+            Logger.getLogger(testManagerDynamique.class.getName()).log(
+                    Level.SEVERE, null, ex);
+            fail(ex.toString());
+        }
+    }
+    
     //////////////////////////////////////////////////////////////////////////
     
-    private void scenarioChercherEau(DonneesSimulation data, Robot robot,
-            int distanceEau, Manager manager)
-            throws SimulationException
-    {
+    private void viderRobot(DonneesSimulation data, Robot robot)
+            throws SimulationException {
         while (!robot.estVide()) {
             robot.deverserEau(data, 10);
         }
         assertThat("Le robot doit initialement être initialement vide",
                 true, is(robot.estVide()));
-
+    }
+    
+    private void scenarioChercherEau(DonneesSimulation data, Robot robot,
+            int distanceEau, Manager manager)
+            throws SimulationException
+    {
         // Le robot est situé à une distance de distanceEau -1 unitées du
         // bord d'eau
         for (int i = 0; i < distanceEau - 1; i++) {
@@ -197,5 +212,52 @@ public class testManagerDynamique {
         manager.manage();
         assertThat("Le feu doit désormais être éteind", 0,
                 is(data.getIncendies().get(0).getIntensite()));
+    }
+
+    private int initCarteMultipleEau(DonneesSimulation data, Case depart)
+            throws ConstructionException {
+        Case eau1, eau2, eau3, eau4;
+        
+        int distanceEau = 10;
+        int x = depart.getColonne();
+        int y = depart.getLigne();
+        
+        eau1 = new Case(x, y + distanceEau, NatureTerrain.EAU);
+        eau2 = new Case(x + 1, y + distanceEau, NatureTerrain.EAU);
+        eau3 = new Case(x + distanceEau, y + distanceEau, NatureTerrain.EAU);
+        eau4 = new Case(x + distanceEau, y, NatureTerrain.EAU);
+        data.addCase(eau1);
+        data.addCase(eau2);
+        data.addCase(eau3);
+        data.addCase(eau4);
+        
+        return distanceEau;
+    }
+
+    private int initCarte1Eau(DonneesSimulation data, Case depart)
+            throws ConstructionException, SimulationException {
+        Case eau;
+        
+        // initalisation de la carte
+        int distanceEau = 10;
+        int x = depart.getColonne();
+        int y = depart.getLigne();
+        eau = new Case(x, y + distanceEau, NatureTerrain.EAU);
+        data.addCase(eau);
+        
+        return distanceEau;
+    }
+
+    private int initCarte1Feu(DonneesSimulation data, Case depart)
+            throws ConstructionException {
+        // initalisation de la carte
+        int distanceFeu = 15;
+        int x = 1;
+        int y = 1;
+        Case feu = new Case(x + distanceFeu, y, NatureTerrain.HABITAT);
+        data.addCase(feu);
+        data.addIncendie(feu, 1); // On initialise un feu simple à éteindre
+        
+        return distanceFeu;
     }
 }
